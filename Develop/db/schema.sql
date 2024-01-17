ce87b717af0ae8ca569e5d2aa671f5b32e739d25
Flesh <olivia.a.gowan@gmail.com>
2024-01-17 15:22:50 -0500

    initial commit

---
+++ Develop/db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE departments (
+	ID INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY(ID)
+);
+
+CREATE TABLE roles (
+	ID INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY(department_id)
+    REFERENCES departments(ID),
+    PRIMARY KEY(ID)
+);
+
+CREATE TABLE employees (
+	ID INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT, 
+    manager_id INT,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (role_id)
+    REFERENCES roles(ID)
+);
