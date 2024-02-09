ffd0af7fa20641b87e34b3603523dfbda4bcac84
Flesh <olivia.a.gowan@gmail.com>
2024-02-08 23:18:28 -0500

    queries and prompts

--- Develop/index.js
+++ Develop/index.js
@@ -32,14 +32,115 @@ const question = [
       "Add a department", 
       "Add a role", 
       "Add an employee", 
-      "Update an employee role"
+      "Update an employee role",
+      "Quit"
     ]
   }
 ];
 
+const newDepartment = [
+  {
+    type: "input",
+    name: "newDepartment",
+    message: "Enter the name of the new department:"
+  }
+]
+
+const newRole = [
+  {
+    type: "input",
+    name: "newRole",
+    message: "Enter the name of the new role:"
+  }
+]
+
+const newEmployee = [
+  {
+    type: "input",
+    name: "newEmployee",
+    message: "Enter the name of the new employee:"
+  }
+]
+
+// Recursive function to repeat the prompt
+function promptUser() {
+  inquirer.prompt(question).then((answers) => {
+    switch (answers.selection) {
+      case "View all departments":
+        // Code to pull departments table from the database
+        db.query('SELECT * FROM departments', function (err, results) {
+          if (err) {
+            console.log(err);
+          }
+          console.log(results);
+          promptUser();
+        });
+        break;
+      case "View all roles":
+        db.query('SELECT * FROM roles', function (err, results) {
+          if (err) {
+            console.log(err);
+          }
+          console.log(results);
+          promptUser();
+        });
+        break;
+      case "View all employees":
+        db.query('SELECT * FROM employees', function (err, results) {
+          if (err) {
+            console.log(err);
+          }
+          console.log(results);
+          promptUser();
+        })
+        break;
+      case "Add a department":
+        inquirer.prompt(newDepartment).then((input) => {
+          db.query(`INSERT INTO departments (name) VALUES (${input})`, function (err, results) {
+            if (err) {
+              console.log(err);
+            }
+            console.log(results);
+          })
+          promptUser();
+        })
+        break;
+      case "Add a role":
+        inquirer.prompt(newRole).then((input) => {
+          db.query(`INSERT INTO departments (name) VALUES (${input})`, function (err, results) {
+            if (err) {
+              console.log(err);
+            }
+            console.log(results);
+            promptUser();
+          })
+        })
+        break;
+      case "Add an employee":
+        inquirer.prompt(newEmployee).then((input) => {
+          db.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (${input});`, function (err, results) {
+            if (err) {
+              console.log(err);
+            }
+            console.log(results);
+            promptUser();
+          })
+        })
+        break;
+      case "Update an employee role":
+        // Code to update an employee's role in the database
+        promptUser();
+        break;
+      case "Quit":
+        console.log("Goodbye!");
+        break;
+    }
+  });
+}
+
 // Initialize app
 function init() {
-  inquirer.prompt(question);
-};
+  promptUser();
+}
 
 init();
